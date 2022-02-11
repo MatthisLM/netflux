f8ad67cbe59487e5c2abd3cb7b525ad0faa18be1
matthislm <matthislm@hotmail.com>
2022-02-11 15:18:12 +0100

    axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+//import '../plugins/axios'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
-createApp(App).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+
+createApp(App).use(store).use(router).use(BootstrapVue).use(BootstrapVueIcons).mount('#app')
