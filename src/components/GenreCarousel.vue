7005b73dcc7564989e6c9c68d07b5fa622a54dc7
matthislm <matthislm@hotmail.com>
2022-02-13 15:51:19 +0100

    carousel section bg

--- src/components/GenreCarousel.vue
+++ src/components/GenreCarousel.vue
@@ -1,235 +1,102 @@
 <template>
-    <div class="container-fluid" :style="{ backgroundImage: 'url(' + require('@/assets/backgrounds/ucm_bg_shape.png') + ')' }">
+    <div class="container-fluid" :style="{ background: 'url(' + require('@/assets/backgrounds/ucm_bg_shape.png') + '), center / cover no-repeat url(https://themebeyond.com/html/movflx/img/bg/ucm_bg.jpg)' }">
+      <div class="grey-overlay">
         <div class="container">
             <div class="row">
                 <div class="col-md-4 text-start">
                     <p class="sub-title fw-bold">ONLINE STREAMING</p>
-                    <h2 class="title display-5">Upcoming Movies</h2>
+                    <h2 class="title display-5 text-white fw-bold">Upcoming Movies</h2>
                 </div>
-                <div class="col-md-8 d-flex"><Button>ok</Button></div>
+                <div class="col-md-8 d-flex align-items-end">
+                  <div class="genre-wrapper">
+                    <Button class="mx-2">ok</Button>
+                    <Button class="mx-2">Test</Button>
+                  </div>
+                </div>
                 <div class="col-xs-11 col-md-12 col-centered">
-
-			<div id="carousel" class="carousel slide" data-ride="carousel" data-type="multi" data-interval="2500">
-				<div class="carousel-inner">
-					<div class="item active">
-						<div class="carousel-col">
-							<div class="block red img-responsive"></div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="carousel-col">
-							<div class="block green img-responsive"></div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="carousel-col">
-							<div class="block blue img-responsive"></div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="carousel-col">
-							<div class="block yellow img-responsive"></div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Controls -->
-				<div class="left carousel-control">
-					<a href="#carousel" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-				</div>
-				<div class="right carousel-control">
-					<a href="#carousel" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
-				</div>
-			</div>
-
-		</div>
+                  
+                </div>
             </div>
+        </div>
         </div>
     </div>
 </template>
 
 <script>
 import Button from '@/components/Button.vue'
+
 export default {
   name: 'GenreCarousel',
   components:{
-      Button 
+    Button,
+  },
+  mounted(){
+    
   }
 }
 </script>
 
 
 <style scoped>
+    .grey-overlay {
+      position: relative;
+      background-color: rgb(20,20,20,0.95);
+      padding: 0 var(--bs-gutter-x,.75rem);
+    }
     .container {
-        background: url('/assets/backgrounds/ucm_bg_shape.png')
+        min-height: 80vh;
+    }
+    .container-fluid {
+        padding: 0;
     }
     .container .sub-title{
         text-transform:uppercase;
         color:var(--secondary-color);
+        margin-top: 48px;
     }
     .container .title{
         font-size:36px;
     }
 
+    .genre-wrapper{
+      margin-bottom: 0.5rem;
+    }
+
+
+    .carousel-nav-icon {
+      height: 48px;
+      width: 48px;
+    }
+    .carousel-item {
+      .col, .col-sm, .col-md {
+        margin: 8px;
+        height: 300px;
+        background-size: cover;
+        background-position: center center;
+      }
+    }
+
+.carousel-item-div {
+  border-radius: 5px;
+}
 
 
 
-    .col-centered {
-  float: none;
-  margin: 0 auto;
+
+/* nothing below this point is needed*/
+@import url('https://fonts.googleapis.com/css?family=Libre+Franklin:300,400,600,700,800,900&display=swap');
+body {
+   background-color: #f2f2f2;
+   color: #171717;
+   height: 100vh;
+   min-height: 100%;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+}
+h1 {
+   font-family: 'Libre Franklin', sans-serif;
+   font-weight: 700;
 }
 
-.carousel-control {
-  width: 8%;
-  width: 0px;
-}
-.carousel-control.left,
-.carousel-control.right {
-  margin-right: 40px;
-  margin-left: 32px;
-  background-image: none;
-  opacity: 1;
-}
-.carousel-control > a > span {
-  color: white;
-  font-size: 29px !important;
-}
-
-.carousel-col {
-  position: relative;
-  min-height: 1px;
-  padding: 5px;
-  float: left;
-}
-
-.active > div {
-  display: none;
-}
-.active > div:first-child {
-  display: block;
-}
-
-/*xs*/
-@media (max-width: 767px) {
-  .carousel-inner .active.left {
-    left: -50%;
-  }
-  .carousel-inner .active.right {
-    left: 50%;
-  }
-  .carousel-inner .next {
-    left: 50%;
-  }
-  .carousel-inner .prev {
-    left: -50%;
-  }
-  .carousel-col {
-    width: 50%;
-  }
-  .active > div:first-child + div {
-    display: block;
-  }
-}
-
-/*sm*/
-@media (min-width: 768px) and (max-width: 991px) {
-  .carousel-inner .active.left {
-    left: -50%;
-  }
-  .carousel-inner .active.right {
-    left: 50%;
-  }
-  .carousel-inner .next {
-    left: 50%;
-  }
-  .carousel-inner .prev {
-    left: -50%;
-  }
-  .carousel-col {
-    width: 50%;
-  }
-  .active > div:first-child + div {
-    display: block;
-  }
-}
-
-/*md*/
-@media (min-width: 992px) and (max-width: 1199px) {
-  .carousel-inner .active.left {
-    left: -33%;
-  }
-  .carousel-inner .active.right {
-    left: 33%;
-  }
-  .carousel-inner .next {
-    left: 33%;
-  }
-  .carousel-inner .prev {
-    left: -33%;
-  }
-  .carousel-col {
-    width: 33%;
-  }
-  .active > div:first-child + div {
-    display: block;
-  }
-  .active > div:first-child + div + div {
-    display: block;
-  }
-}
-
-/*lg*/
-@media (min-width: 1200px) {
-  .carousel-inner .active.left {
-    left: -25%;
-  }
-  .carousel-inner .active.right {
-    left: 25%;
-  }
-  .carousel-inner .next {
-    left: 25%;
-  }
-  .carousel-inner .prev {
-    left: -25%;
-  }
-  .carousel-col {
-    width: 25%;
-  }
-  .active > div:first-child + div {
-    display: block;
-  }
-  .active > div:first-child + div + div {
-    display: block;
-  }
-  .active > div:first-child + div + div + div {
-    display: block;
-  }
-}
-
-.block {
-  width: 306px;
-  height: 230px;
-}
-
-.red {
-  background: red;
-}
-
-.blue {
-  background: blue;
-}
-
-.green {
-  background: green;
-}
-
-.yellow {
-  background: yellow;
-}
 </style>
